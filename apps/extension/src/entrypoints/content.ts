1e50e23ee58da2c37929ff9c26d74f3077aa79aa
AnasSiddiqui18 <anassiddiqui0910@gmail.com>
2025-10-06 14:54:15 +0500

    fix: ensure events dispatched correctly

--- apps/extension/src/entrypoints/content.ts
+++ apps/extension/src/entrypoints/content.ts
@@ -27,16 +27,19 @@ export default defineContentScript({
                                 )
                             ) {
                                 console.log("input found", node);
+                                targetEl = node;
                                 observer.disconnect();
                             } else {
                                 for (const selector of possibleSelectors) {
                                     const target = node.querySelector(
                                         selector,
                                     ) as HTMLInputElement;
+
                                     if (target) {
                                         const url = window.location.href
                                             .split("?")
                                             .at(0);
+
                                         if (!url)
                                             return console.error(
                                                 "Url not found",
@@ -69,6 +72,8 @@ export default defineContentScript({
             const { token } = data;
             if (!targetEl) return console.error("Target element not found");
             targetEl.value = token;
+            targetEl.dispatchEvent(new Event("input", { bubbles: true }));
+            targetEl.dispatchEvent(new Event("change", { bubbles: true }));
         });
     },
 });
